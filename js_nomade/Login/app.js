0e705f76b33ff97a4e30a81e5825a2cb56148365
josuhun <chosh9128@gmail.com>
2023-10-20 00:02:51 +0900

    23_10_20

--- js_nomade/Login/app.js
+++ js_nomade/Login/app.js
@@ -1,20 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
-const link = document.querySelector('a');
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 
 function onLoginSubmit(event) {
     event.preventDefault()
-    // const username = loginInput.value;
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-link.addEventListener('click', handleLinkClick)
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername)
+}
+
