eaf135a78279f1f861aa82c71a2875fcd85d6a82
josuhun <chosh9128@gmail.com>
2023-10-30 00:35:25 +0900

    js_23_10_30

--- js_nomade/Todo/js/background.js
+++ js_nomade/Todo/js/background.js
@@ -2,8 +2,4 @@ const images = ["1.jpeg","2.jpeg","3.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `image/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.setAttribute("background", `image/${chosenImage}`)
